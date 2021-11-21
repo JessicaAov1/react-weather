c87e4ec8dc0d565f455110fbf83414f9a32576c0
JessicaAov1 <86833978+JessicaAov1@users.noreply.github.com>
2021-11-21 02:00:16 +0000

    Refactored code and changed the css

--- src/Forecast.js
+++ src/Forecast.js
@@ -15,7 +15,15 @@ export default function Forecast(props) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
-
+  function load() {
+    console.log(forecast);
+    const ApiKey = `9ca6c562062d122440b16668ce916487`;
+    const latitude = props.coordinates.lat;
+    const longitude = props.coordinates.lon;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${ApiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
   if (loaded) {
     return (
       <div className="WeatherForecast">
@@ -33,12 +41,7 @@ export default function Forecast(props) {
       </div>
     );
   } else {
-    console.log(forecast);
-    const ApiKey = `9ca6c562062d122440b16668ce916487`;
-    const latitude = props.coordinates.lat;
-    const longitude = props.coordinates.lon;
-    const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${ApiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+    load();
     return null;
   }
 }
